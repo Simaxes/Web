3ceb0e95d01cee9cde624a686d3efc0433a8000b
Simaxes <simaxes@gmail.com>
2018-05-12 01:35:14 +0300

    first commit

---
+++ Lab4/education/js/common.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function()
+{
+    (function ($)
+    {
+        $(function ()
+        {
+            $("select").styler
+            ({
+                selectSearch: false
+            });
+
+            //$('.rating').styler('destroy');
+        });
+    })(jQuery);
+});
+
+
